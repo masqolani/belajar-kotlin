41963f1279105bd39cccd208fae89f966c8ea33b
Muhammad Asqolani <m.asqolani@gmail.com>
2021-07-25 21:57:08 +0700

    add contructor and modify gitignore

--- src/main/kotlin/app/entity/Car.kt
+++
@@ -1,9 +0,0 @@
-package app.entity
-
-/**
- * Constructor entity example
- */
-class Car(brand: String, year: Int = 2020) {
-    var brand: String = brand
-    var year: Int = year
-}
--- src/main/kotlin/basic/datatype/Boolean.kt
+++
@@ -1,9 +0,0 @@
-package basic.datatype
-
-fun main() {
-    var bolTrue: Boolean = true;
-    var bolFals: Boolean = false;
-
-    println(bolTrue)
-    println(bolFals)
-}
